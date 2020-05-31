9ea6d9b879b429b44168be3a5cb9116921ed6112
cuilihuan <1738127840@qq.com>
2020-05-31 09:58:07 +0800

    提交链表之两数相加(补充递归法)

--- src/main/java/链表/_2_两数相加.java
+++ src/main/java/链表/_2_两数相加.java
@@ -6,6 +6,13 @@ package 链表;
  * 属于简单类型 只需要把各种情况考虑进去即可
  */
 public class _2_两数相加 {
+
+    /**
+     * 非递归法
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode newListNode = new ListNode(0);
         ListNode temp = newListNode;
@@ -27,4 +34,25 @@ public class _2_两数相加 {
         if(carray>0)  newListNode.next = new ListNode(carray);
         return temp.next;
     }
+
+    /**
+     * 递归法
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        return add(l1, l2, 0);
+    }
+
+    public ListNode add(ListNode l1, ListNode l2, int a) {
+        if(l1 == null && l2 == null && a == 0) return null;
+        if(l1 == null) l1 = new ListNode(0);
+        if(l2 == null) l2 = new ListNode(0);
+        int sum = l1.val + l2.val + a;
+        ListNode head = new ListNode(sum % 10);
+        head.next = add(l1.next, l2.next, sum / 10);
+        return head;
+    }
+
 }
